a129b570494d829f68b544bccfe5ff22c17f7b7f
imane <imanezenasnii@gmail.com>
2024-10-17 18:51:04 +0100

    Add prime game project

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" is Winner """
+
+
+def isWinner(x, nums):
+    """ isWinner(x, nums) """
+
+    def GetPrimes(max_num):
+        """ GetPrimes """
+        is_prime = [True] * (max_num + 1)
+        p = 2
+        while (p * p <= max_num):
+            if is_prime[p]:
+                for i in range(p * p, max_num + 1, p):
+                    is_prime[i] = False
+            p += 1
+        return is_prime
+    if x <= 0 or not nums:
+        return None
+
+    max_n = max(nums)
+    is_primeNumber = GetPrimes(max_n)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n == 1:
+            ben_wins += 1
+        else:
+            primes = []
+            for i in range(2, n + 1):
+                if is_primeNumber[i]:
+                    primes.append(i)
+            if len(primes) % 2 == 0:
+                ben_wins += 1
+            else:
+                maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,14 @@
+# Prime Game
+
+## Description
+
+The Prime Game is a competitive game where two players, Maria and Ben, take turns selecting prime numbers from a set of consecutive integers starting from 1 up to a given number \( n \). The selected prime number and all of its multiples are removed from the set. The player who cannot make a move loses the game. Maria always plays first, and both players play optimally.
+
+This project involves determining the winner of multiple rounds of the game based on varying values of \( n \). The goal is to find out who wins the most rounds.
+
+## Requirements
+
+- The implementation must be done in Python 3 (version 3.4.3).
+- The code must follow the PEP 8 style guidelines.
+- Each file should be executable and must end with a new line.
+- The first line of each Python file should be exactly `#!/usr/bin/python3`.
